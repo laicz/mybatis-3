a9736d92fe6592453fed2d1251aebc317c9a50dc
zhoumb <zhoumb@vibeac.com>
2019-04-09 21:05:21 +0800

    反射模块的注释

--- src/main/java/org/apache/ibatis/reflection/DefaultReflectorFactory.java
+++ src/main/java/org/apache/ibatis/reflection/DefaultReflectorFactory.java
@@ -1,48 +1,57 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * 默认的Reflector工厂，默认提供缓存Reflector的作用
+ */
 public class DefaultReflectorFactory implements ReflectorFactory {
-  private boolean classCacheEnabled = true;
-  private final ConcurrentMap<Class<?>, Reflector> reflectorMap = new ConcurrentHashMap<>();
+    /**
+     * 是否缓存对象的Reflector对象
+     */
+    private boolean classCacheEnabled = true;
+    /**
+     * Reflector缓存对象集合
+     */
+    private final ConcurrentMap<Class<?>, Reflector> reflectorMap = new ConcurrentHashMap<>();
 
-  public DefaultReflectorFactory() {
-  }
-
-  @Override
-  public boolean isClassCacheEnabled() {
-    return classCacheEnabled;
-  }
-
-  @Override
-  public void setClassCacheEnabled(boolean classCacheEnabled) {
-    this.classCacheEnabled = classCacheEnabled;
-  }
-
-  @Override
-  public Reflector findForClass(Class<?> type) {
-    if (classCacheEnabled) {
-      // synchronized (type) removed see issue #461
-      return reflectorMap.computeIfAbsent(type, Reflector::new);
-    } else {
-      return new Reflector(type);
+    public DefaultReflectorFactory() {
     }
-  }
+
+    @Override
+    public boolean isClassCacheEnabled() {
+        return classCacheEnabled;
+    }
+
+    @Override
+    public void setClassCacheEnabled(boolean classCacheEnabled) {
+        this.classCacheEnabled = classCacheEnabled;
+    }
+
+    @Override
+    public Reflector findForClass(Class<?> type) {
+        if (classCacheEnabled) {
+            // synchronized (type) removed see issue #461
+            return reflectorMap.computeIfAbsent(type, Reflector::new);
+        } else {
+            return new Reflector(type);
+        }
+    }
 
 }
